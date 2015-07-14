dbb0a860a4026b34ee8b4b1522347d4a41c66ed1
John Andersen <johnandersenpdx@gmail.com>
2015-07-14 15:53:42 -0700

    Swarm launcher working

---
+++ proxy.py
@@ -0,0 +1,8 @@
+import stratus
+
+class service(stratus.stratus):
+    """
+    The proxy service for micropython
+    """
+    def __init__(self):
+        super(service, self).__init__()
--- swarm.py
+++ swarm.py
@@ -1,6 +1,34 @@
+import sys
+import uuid
 import stratus
 
+from config import config
 import proxy
-import db
 import web
 import api
+import db
+
+def launch(service):
+    # Get the service
+    module = getattr(sys.modules[__name__], service)
+    to_launch = getattr(module, "service")
+    # Set the options
+    options = {
+        # Random name so all services dont connect with same hostname
+        "name": str(uuid.uuid4()),
+        # Specify the service
+        "service": service
+    }
+    # Create the service
+    to_launch = to_launch()
+    # Start the service
+    to_launch.start(**options)
+
+def main():
+    launch("proxy")
+    while True:
+        pass
+    return
+
+if __name__ == '__main__':
+    main()
